6c0c9bd3748d3ccd2673b09cfa5e635aaea30d8c
Cho jeonghyun <peterchokr@gmail.com>
2023-09-06 16:57:50 +0900

    .

--- src/chap04/p114_02_block.py
+++
@@ -1,7 +0,0 @@
-# 블록
-
-if  price > 20000 :
-	shipping_cost = 0
-	discount = 0.1
-else :
-	shipping_cost = 3000
--- src/chap05/p159_else.py
+++
@@ -1,7 +0,0 @@
-i = 0
-
-while i < 3:
-    print("루프 안쪽")
-    i = i + 1
-else:
-    print("else 부분")
